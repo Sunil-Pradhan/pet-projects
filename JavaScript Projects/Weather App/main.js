c8360ff3688b2e0daa96ff43bdf396d5235404c9
Sunil-Pradhan <pradhan.sunil@outlook.com>
2023-07-22 22:22:32 +0530

    js project

--- JavaScript Projects/Weather App/main.js
+++ JavaScript Projects/Weather App/main.js
@@ -2,4 +2,15 @@ const apiKey = '570cac01635c32fa3dbebcaf72b0a598';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore';
 
 
+async function checkWeather() {
+	const response = await fetch(apiUrl + `&appid=${apiKey}`);
+	var data = await response.json();
+	console.log(data);
 
+	document.querySelector('.city').innerHTML = data.name;
+	document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c';
+	document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+	document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
+
+}
+checkWeather();
