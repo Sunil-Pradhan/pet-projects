9a4144cfb07deccda5cadc8bd97b7e7fc193b039
Sunil-Pradhan <pradhan.sunil@outlook.com>
2023-08-13 01:09:57 +0530

    JavaScript Form Validation

--- JavaScript Projects/JavaScript Form Validation/main.js
+++ JavaScript Projects/JavaScript Form Validation/main.js
@@ -15,6 +15,10 @@ let failureIcon = classes("failure-icon");
 // Target the form and add the submit event listener
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
+
+	engine(username, 0, 'Username cannot be blank');
+	engine(email, 1, 'Email cannot be blank');
+	engine(password, 2, 'Password cannot be blank');
 })
 
 // Create a function named engine which will do
@@ -27,10 +31,20 @@ form.addEventListener('submit', (e) => {
 
 let engine = (id, serial, message) => {
 	if (id.value.trim() === "") {
+		errorMsg[serial].innerHTML = message;
+		id.style.border = '2px solid red';
 
+		//icons
+		failureIcon[serial].style.opacity = "1";
+		successIcon[serial].style.opacity = "0";
 	}
 	else {
+		errorMsg[serial].innerHTML = "";
+		id.style.border = "2px solid green";
 
+		// icons
+		failureIcon[serial].style.opacity = "0";
+		successIcon[serial].style.opacity = "1";
 	}
 }
 
